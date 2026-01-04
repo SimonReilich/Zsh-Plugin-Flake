837fa471eb20242b71c6fd70c775987a60c6a50a
SimonReilich <simon.reilich@proton.me>
2026-01-04 13:02:44 +0100

    simplified attribute names

--- flake.nix
+++ flake.nix
@@ -2,17 +2,17 @@
   description = "A flake providing a collection of zsh plugins";
 
   inputs = {
-    zsh-syntax-highlighting = {
+    syntax-highlighting = {
       url = "github:zsh-users/zsh-syntax-highlighting";
       flake = false;
     };
 
-    zsh-completions = {
+    completions = {
       url = "github:zsh-users/zsh-completions";
       flake = false;
     };
 
-    zsh-autosuggestions = {
+    autosuggestions = {
       url = "github:zsh-users/zsh-autosuggestions";
       flake = false;
     };
